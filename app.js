79f73dec1cf993f5c95f5e63506f773b64c46788
Sharon Wag <sharon.ran.wang@gmail.com>
2015-10-29 20:43:49 -0400

    seems to work...

--- app.js
+++ app.js
@@ -1,62 +1,55 @@
-//figure out algorithmic complexity
-//test ALL EDGE CASES
-//print with /n newline
-
-//comment out code
-
-//write solution to stdout
-
 var _ = require('underscore');
 process.stdin.setEncoding('utf8'); 
 
-process.stdin.on('data', function(data){
+process.stdin.on('data', function(data){ //reading data from input
 	var logArr = data.split("\n");
-	var count = logArr[0]
+	var count = parseInt(logArr[0]) //grabbing N from the first line
 
 	var pairs = [];
-	var qualifyingPairs = [];
-	// console.log(findPairs(logArr[1].split(',')))
+	var qualifyingPairs = "";
 
-	for (var i=1; i<logArr.length; i++){
-		findPairs(logArr[i].split(','));
+	for (var i=1; i<logArr.length; i++){ // sort logs line by line
+		var individualLog = _.uniq(logArr[i].split(',').sort(), true);
+		findPairs(individualLog);
 	}
-
-	console.log(pairs);
 
 	function findPairs(set){ // find all possible pairs
 		if (set.length < 2) { 
 			return
 		} else if (set.length === 2) {
-			pairs.push([set[0] + ", " + set[1]]);
+			pairs.push([set[0] + "," + set[1]]);
 		} else {
 			for (var i = 0; i < set.length - 1; i++){
 				for (var j = 1; j < set.length - i; j++){
-					pairs.push(set[i] + ", " + set[i+j]);
+					pairs.push(set[i] + "," + set[i+j]);
 				}
 			}
 		}			
 	}
 
-	_.groupBy(pairs, function(num){ r
-		eturn Math.floor(num); 
-	});
+	pairs = pairs.sort(); 
 
-	// process.stdout.write(sortedPairs + '\n');
+	var pairCount = 1; //store a pairCount variable to see how many instances of the same pair occur
+	var prevPair = pairs[0];
+
+	for (var i=1; i<pairs.length; i++){ 
+		if (pairs[i] === prevPair) {
+			pairCount ++;
+			if (pairCount === count) { //write to string if N instances of pair
+				qualifyingPairs += pairs[i] + "\n"
+			}
+		} else {
+			pairCount = 1;
+			prevPair = pairs[i];
+		}
+	}
+
+	process.stdout.write(qualifyingPairs);
 })
 
-// var readline = require('readline'); //allow to read line by line
-// var rl = readline.createInterface({
-//   input: process.stdin,
-//   output: process.stdout,
-//   terminal: false
-// });
-
-// rl.on('line', function(line){
-// 	console.log(line)
-// })
-
-
-// process.stdout.write()
+//Extra
+//time complexity is 0(n)^3, deepest is 3 nested loops (at line 20)
+//not sure, this is the solution I came up with
 
 
 
